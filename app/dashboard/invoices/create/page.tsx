04d62f611d2c030128a7128ebdb27260244f68c0
TheRebellionDreamer <rebellion.dreamer@yandex.ru>
2023-12-07 08:48:00 +0300

    update(Урок 14. Валидация форм и доступность)

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,7 +1,6 @@
-import Form from '@/app/ui/invoices/create-form';
-import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers } from '@/app/lib/data';
-import Link from 'next/link';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import Form from '@/app/ui/invoices/create-form';
 
 export default async function Page() {
   const customers = await fetchCustomers();
@@ -19,7 +18,6 @@ export default async function Page() {
         ]}
       />
       <Form customers={customers} />
-      <Link href="/dashboard/invoices/create/modal">Open</Link>
     </main>
   );
 }
